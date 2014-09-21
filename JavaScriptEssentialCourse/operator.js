4d9c0dd9471c67055679a016d792bdd306592ae2
An Tran Minh <atran@atlassian.com>
2014-09-21 20:41:40 +0700

    test operators

---
+++ JavaScriptEssentialCourse/operator.js
@@ -0,0 +1,14 @@
+(function()
+{
+    var a = 1;
+    var b = 2;
+    var c = a + b;
+    
+    console.log(c);
+    
+    var d = "my name is ";
+    var e = "An Tran";
+    var f = d + e;
+    console.log(f);
+    
+})();

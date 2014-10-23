a0cfc96cc7bc9f3633d09f61268bcc0630f0c6f9
An Tran Minh <atran@atlassian.com>
2014-10-24 00:08:25 +0700

    steoreo code layout

---
+++ angularjs/AngularJS_Essentials/chap02/steoreo/js/controllers.js
@@ -0,0 +1,27 @@
+parking.controller("parkingCtrl", function($scope){
+    $scope.cars = [
+        {plate: '6MBV006', entrance: new Date(), color: 'Red'},
+        {plate: '5BBM299', entrance: new Date(), color: 'Green'},
+        {plate: '5AOJ230', entrance: new Date(), color: 'Blue'}
+    ];
+
+    $scope.colors = ["White", "Black", "Blue", "Red", "Silver"];
+
+    $scope.park = function(car) 
+    {
+        car.entrance = new Date();
+        $scope.cars.push(angular.copy(car));
+        delete $scope.car;
+    };
+
+    $scope.remove = function(car) 
+    {
+        for (var i = 0; i < $scope.cars.length; i++) 
+        {
+            if ($scope.cars[i].plate == car.plate)
+            {
+                $scope.cars.splice(i, 1);
+            }
+        }                   
+    };
+});
